fc3bf103a01bdce99c6d33f2e155f7c257167d25
Owen Thomas <owen@owenhthomas.com>
2018-08-02 13:34:39 -0700

    Package inside a yeoman app
    yo webapp
    Then move all files/folders for html, images, scripts, css, etc. into the app/ directory.

--- app/js/restaurant_info.js
+++ app/js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoib3Rob21hczE5ODQiLCJhIjoiY2ppYjh6a2M2MDQyNDNwcXJxdWNybjQ1YyJ9.LlwJqcW68IZ9HEoDmR64Kw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

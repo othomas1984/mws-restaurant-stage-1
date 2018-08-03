59ff1413cf06dfba2f3346578349b04885a69103
Owen Thomas <owen@owenhthomas.com>
2018-08-02 18:51:22 -0700

    Store JSON restaurant data response in IDB and fetch from there first

--- app/sw.js
+++ app/sw.js
@@ -1,3 +1,11 @@
+import idb from 'idb';
+
+var dbPromise = idb.open('mws-restaurant-app', 1, (upgrade) => {
+  switch (upgrade.oldVersion) {
+    case 0:
+      upgrade.createObjectStore('restaurants');
+  }
+});
 
 var currentCacheName = 'restaurants-cache-0001';
 
@@ -10,7 +18,16 @@ self.addEventListener('install', (event) => {
         '/js/restaurant_info.js',
         '/css/styles.css',
         '/index.html',
-        '/restaurant.html'
+        '/restaurant.html',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg'
       ]);
     }).catch ( (error) => {
       console.log("Cache Error: ", error)
@@ -37,8 +54,12 @@ self.addEventListener('fetch', (event) => {
   var url = new URL(event.request.url); 
   // console.log('Fetch caught', url); 
 
-
   let request = event.request 
+  if (url.port == 1337) {
+    handleRestaurantDataRequest(event, url.pathname)
+    return
+  }
+
   if (url.origin === location.origin) {
     if (url.pathname === '/restaurant.html') {
       request = '/restaurant.html'
@@ -46,14 +67,47 @@ self.addEventListener('fetch', (event) => {
     if (url.pathname === '/') {
       request = '/index.html'
     }
+    if (url.pathname === '/img/undefined.jpg') {
+      request = '/img/1.jpg'
+    }
   }
   event.respondWith(
     caches.match(request).then( (response) => {
       if (response) console.log('Serving response from cache', event.request.url);
-      return response || fetch(event.request);
+      return response || fetch(event.request).then( response => {
+        if (response.status == 404) {
+          return fetch(new URL('http://localhost:9000/img/1.jpg'))
+        }
+        return response;
+      }).catch( error => {
+        console.log("Fetch Error:", error)
+      })
     })
   );
 });
+
+var handleRestaurantDataRequest = (event, key) => {
+  event.respondWith(
+    dbPromise.then(db => {
+      return db.transaction('restaurants').objectStore('restaurants').get(key).then( cachedResponse => {
+        if (cachedResponse) console.log('Serving response from cache', event.request.url);
+        return cachedResponse || fetch(event.request).then(fetchResonse => {
+          return fetchResonse.json().then( json => {
+            const tx = db.transaction('restaurants', 'readwrite');
+            tx.objectStore('restaurants').put(json, key);
+            tx.complete;
+            return json
+          })
+        });
+      });
+    }).then( json => {
+      let response = new Response(JSON.stringify(json));
+      return response
+    }).catch(error => {
+      console.log("IndexedDB Error:", error);
+    })
+  );
+}
 
 self.addEventListener('message', (event) => {
   console.log('Skip Waiting command sent')
